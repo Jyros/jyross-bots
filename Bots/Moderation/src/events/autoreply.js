7c98df161ca60a494e1e97606f1e879239f3ef2f
Jyros <77053154+Jyros@users.noreply.github.com>
2021-06-29 12:29:02 +0300

    Update autoreply.js

--- Bots/Moderation/src/events/autoreply.js
+++ Bots/Moderation/src/events/autoreply.js
@@ -8,7 +8,7 @@ if (selamlar.some(s => message.content.toLowerCase() === s)) {
 message.channel.send(`${message.author}, Aleyküm selam hoş geldin.`)}
 let cezapuan = await kdb.fetch(`cezapuan.${message.author.id}`) || "0"
 if([".cezapuan","cezapuan","ceza-puan"].includes(message.content.toLowerCase())){ 
-return message.channel.send(`${message.author}, toplam ceza puanınız: ${cezapuan}`)}}
+return message.channel.send(`${message.author}, toplam ceza puanınız: ${cezapuan}`)}
 };
 module.exports.conf = {
 name: "message"
