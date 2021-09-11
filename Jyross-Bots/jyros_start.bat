dc97d8772146492e58cdf2b7774a08727bcf348f
Jyross <77053154+Jyros@users.noreply.github.com>
2021-09-11 21:40:56 +0300

    Delete Jyross-Bots directory

--- Jyross-Bots/jyros_start.bat
+++
@@ -1,7 +0,0 @@
-echo off
-color 0f
-title Jyros Bots
-cls
-:a
-node jyros.js
-goto a
--- Jyross-Bots/src/operations/functions.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) => {
-    
-      client.wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
- 
-      Array.prototype.random = function () {
-        return this[Math.floor((Math.random() * this.length))];
-      };
-
-}
